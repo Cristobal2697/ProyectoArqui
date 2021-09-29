dab218e0face4d90c548b782194422f81aca0b8b
Cristobal2697 <37197715+Cristobal2697@users.noreply.github.com>
2021-09-29 17:50:47 -0600

    Update discograficas_index.vue

--- pages/discograficas_index.vue
+++ pages/discograficas_index.vue
@@ -14,11 +14,26 @@
    <HeaderView />
    <h3 style="margin-top: 15px">Discograficas Information</h3>
      <p>This section presents information about discograficas</p>
-   <ul>
-     <li v-for="d of discograficas" :key="d.slug">
-       <NuxtLink :to="{ name: 'discograficas-slug', params: { slug: d.slug } }">{{d.name}}</NuxtLink>
-     </li>
-   </ul>
+     <ul class="ul">
+          <li v-for="d of discograficas" :key="d.slug">
+            <div class="card">
+              <div>
+                <img class="card-img" :src="'/images/' + d.image" />
+              </div>
+              <div class="card-text">
+                <h4 class="card-title"><strong> {{ d.name }} </strong></h4>
+                <p class="card-description">
+                  <span>País: {{d.country}}</span><br>
+                  <span>Fundadación: {{d.founded}}</span><br>
+                  <span>Género: {{d.genere}}</span><br>
+                </p>
+                <NuxtLink :to="{ name: 'discograficas-slug', params: { slug: d.slug } }" class="button">
+                    Detalles
+                </NuxtLink>
+              </div>
+            </div>
+          </li>
+        </ul>
    <FooterView />
  </div>
 </template>
